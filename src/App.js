81e07d0521a93a66dc47a450d0bc30530e89fc14
Manan Jain <mananjain31jan@gmail.com>
2022-01-21 23:26:30 +0530

    :hammer: parcel setup for react project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const App = () => {
+  return <div className="app">
+      <h1>React Protected Routes</h1>
+      <hr width="90%"/>
+
+
+
+  </div>;
+}
+
+export default App;
